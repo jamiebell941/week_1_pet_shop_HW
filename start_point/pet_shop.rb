0ab675ef1c65af09b47dc051ca18fcb84923ff9c
jamiebell941 <jamiebell941@gmail.com>
2020-02-23 21:38:04 +0000

    fixed problem in remove pet

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,16 +1,19 @@
+### find value from hash
 def pet_shop_name(shop_name)
-  for name in @pet_shop
-    @pet_shop.find "Camelot of Pets"
-  end
-p @pet_shop[:name]
+  # for name in @pet_shop
+  #   @pet_shop.find "Camelot of Pets"
+  # end
+return @pet_shop[:name]
 end
 # def pet_shop_name(name)
 # pet_shop.find{|name| @pet_shop[:name] == "Camelot of Pets"}
 # end
+
+### find value from hash
 def total_cash(sum)
  return sum[:admin][:total_cash]
   end
-
+###
 def add_or_remove_cash(pet_shop, cash)
 # cash = @pet_shop.map{|cash| @pet_shop[:admin][:total_cash]
  @pet_shop[:admin][:total_cash] += cash
@@ -45,35 +48,54 @@ end
   # @pet_shop[:pets][:breed].count
 
 def find_pet_by_name(pet_shop, name)
-  for pet in @pet_shop[:pets]
+  for pet in pet_shop[:pets]
       return pet if name == pet[:name]
     end
       return nil
 end
 
-# def remove_pet_by_name(pet_shop,name)
-#   for pet in @pet_shop[:pets]
-#   return  pet.delete[:name] if name == pets[:name]
+# def remove_pet_by_name(pet_shop, name_remove)
+#    pet_remove = find_pet_by_name(pet_shop, name_remove)
+#    pet_shop[:pets].delete(pet_remove)
+#    return find_pet_by_name
 #   end
+
+#
+
+# def remove_pet_by_name(pet_shop, name)
+#   # @pet_shop[:name].delete(pet[:name])
+#   # return nil
+#  @pet_shop[:pets][:name].sort
+#  for pet in pets
+#   if name == pet[:name]
+#     return pet[:name].shift
+#   end
+# return nil
+# end
+# end
+# delete pet from pets
+
+# if name is equal to Pet
+# elseif return
 # end
 
-def remove_pet_by_name(pet_shop, name)
-  # @pet_shop[:name].delete(pet[:name])
-  # return nil
- @pet_shop[:pets][:name].sort
-  if name == pets[:name]
-    pet[:name].shift
-    return
-  end
-return nil
-end
+# def remove_pet_by_name(pet_shop, name)
+#   # pets[:name].delete(name)
+#   return pets[:name].delete(name)
+# end
 
+def remove_pet_by_name(pet_shop, name_remove)
+   pet_remove = find_pet_by_name(pet_shop, name_remove)
+   pet_shop[:pets].delete(pet_remove)
+  end
 
 def add_pet_to_stock(pet_shop,new_pet)
     return @pet_shop[:pets].push(@new_pet)
   end
 
-
+  def customer_cash(customers)
+    return customers[:cash]
+  end
     # def customer_cash(customer)
       # return sum[:cash]
     #   customer_cash = 0
@@ -81,6 +103,28 @@ def add_pet_to_stock(pet_shop,new_pet)
     #     cash += customer[:cash]
     #   end
     # end
-    def customer_cash(customers)
-      return @customers[:cash]
+    # def customer_cash(customers)
+    #   return @customers[:cash]
+    # end
+
+    def remove_customer_cash(customer, cash_remove)
+      customer[:cash] -= cash_remove
     end
+
+    def customer_pet_count(customers)
+      return customers[:pets].count
+    end
+
+def add_pet_to_customer(customer, new_pet)
+  return customer[:pets].push(new_pet).count
+end
+
+def customer_can_afford_pet(customer, new_pet)
+  if customer[:cash] >= new_pet[:price]
+    return true
+      else customer[:cash] < new_pet[:price]
+        return false
+      # elsif customer[:cash] == new_pet[:price]
+      #   return true
+  end
+end
